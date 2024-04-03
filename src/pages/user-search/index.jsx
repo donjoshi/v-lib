c7636840cf6995698f9cf01ba7962a5e32a6388e
Don Joshi N <97145305+donjoshi@users.noreply.github.com>
2024-04-03 09:41:48 +0530

    committed

--- src/pages/user-search/index.jsx
+++ src/pages/user-search/index.jsx
@@ -1,10 +1,70 @@
 import React from "react";
 import NavbarMain from "../../components/navbar-main";
 import "./styles.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 
-export default function UserSearch()
-{
+export default function UserSearch() {
+
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const [text_area, setText_area] = useState('');
+    const [genre, setGenre] = useState('');
+
+    const navigate = useNavigate();
+
+
+
+    async function handleOnSubmit(event) 
+    {
+        event.preventDefault(); // Prevent default form submission
+
+        setLoading(true); // Set loading state
+
+        const formData = new FormData();
+        
+        try
+        {
+            formData.append('text_area', text_area);
+            // formData.append('genre', genre);
+
+
+            var requestOptions1 = {
+                method: 'POST',
+                body: formData, 
+                headers: {
+                    'Origin': 'http://192.168.1.75:8000' // Replace with your React app's origin
+                  },
+                // redirect: 'follow'
+            };
+
+            
+
+            const response = await fetch("api", requestOptions1)
+
+
+            const data = await response.text();
+            // navigate("/upload-success")
+            console.log(data);
+        }
+
+        catch (error)
+        {
+            console.error(error);
+            setError('Error submitting form.');
+            <h1>Error Occurred</h1>
+
+            setLoading(false); // Reset loading state
+
+        }
+
+    
+    }
+
+
+
 
     return (
 
@@ -17,19 +77,25 @@ export default function UserSearch()
                 <div className="text">Get contextual results</div>
 
                 <div className="search-space">
-                    <div className="search-space-1">
-                        <div className="content">
-                            <textarea class="text-area" placeholder="Paste your content"></textarea>
-                        </div>
-                        <div className="genre">
-                            <label htmlFor="genre">Genre</label>
-                            <input type="text" placeholder="Type genres you are searching for(optional)"/>
-                        </div>
-                    </div>
+                    <form onSubmit={handleOnSubmit}>
+                        <div className="search-space-1">
+                            <div className="content">
+                                <textarea className="text-area" placeholder="Paste your content" onChange={e=>setText_area(e.target.value)}></textarea>
+                            </div>
+                            <div className="genre">
+                                <label htmlFor="genre">Genre</label>
+                                <input type="text" placeholder="Type genres you are searching for(optional)" onChange={e=>setGenre(e.target.value)}/>
+                            </div>
 
-                    <div className="search-space-2">
-                        <button>Search</button>
-                    </div>
+                            <div className="search-space-2">
+                                <button type="submit" disabled={loading}>
+                                    {loading ? "Searching...." : "Search"}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+
+
 
                 </div>
             </div>
