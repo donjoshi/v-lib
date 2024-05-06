a03429a82e818f90349b6ebf4ea0ad82c5bdc63d
Christin Sibi <christinpsibi333@gmail.com>
2024-05-06 19:47:32 +0530

    Update consts.js

--- src/pages/consts.js
+++ src/pages/consts.js
@@ -1,9 +1,9 @@
-const IP_ADDRESS = 'http://127.0.0.1:8000';
+// const IP_ADDRESS = 'http://127.0.0.1:8000';
 
 <<<<<<< Updated upstream
 // const IP_ADDRESS = 'http://192.168.1.61:8000';
 =======
-// const IP_ADDRESS = 'http://192.168.1.75:8000';
+const IP_ADDRESS = 'http://192.168.1.75:8000';
 >>>>>>> Stashed changes
 
 
